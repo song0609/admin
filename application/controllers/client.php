0f1ad8651453ad35ba23de22f01a5334d9a213f8
songjian <songjian0609@qq.com>
2017-01-16 19:51:31 +0800

    list

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -20,4 +20,28 @@ class Client extends Client_Controller {
     public function test(){
         echo "1111";exit;
     }
+
+    public function getFinanceList(){
+        $this->load->helper('page');
+        $this->load->model(array('MFinance','MClient'),'',TRUE);
+        $page = $this->input->get('per_page');
+        $page = !empty($page)?$page:1;
+        $username = $this->session->userdata('username');
+        $client = $this->MClient->getClientByUsername($username);
+        $client_id = $client[0]['id'];
+        $total = $this->MFinance->getTotalCount(array('client_id'=>$client_id));
+        $pagesize = 10;
+        $offset = $pagesize*($page-1);
+        $data = $this->MFinance->getFinanceList($offset,$pagesize,array('client_id'=>$client_id));
+        $clients = $this->MClient->getAdvertiserList(0,100);
+        $clients_arr = array();
+        foreach($clients as $val){
+            $clients_arr[$val['id']] = $val;
+        }
+        $arr['data'] = $data;
+        $arr['clients'] = $clients_arr;
+        $arr['pagination'] = pagination(site_url("c=admin&m=getFinanceList"),$pagesize,$total);
+        $arr['total'] = $total;
+        $this->load->view('admin/finance_list',$arr);
+    }
 }
